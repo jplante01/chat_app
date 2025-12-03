45db1ea69bc0dca7bdf0a8995145bf875deaa510
Jeffrey Plante <git@jplante.dev>
2025-12-03 15:40:09 -0800

    Add profile trigger

--- supabase/migrations/20251126220948_initial_schema.sql
+++ supabase/migrations/20251126220948_initial_schema.sql
@@ -69,3 +69,21 @@ CREATE TRIGGER update_conversation_on_message_update
   AFTER UPDATE ON messages
   FOR EACH ROW
   EXECUTE FUNCTION update_conversation_timestamp();
+
+-- Function to create profile when user signs up
+CREATE FUNCTION public.handle_new_user()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+SECURITY DEFINER SET search_path = ''
+AS $$
+BEGIN
+  INSERT INTO public.profiles (id, username)
+  VALUES (NEW.id, NEW.raw_user_meta_data ->> 'username');
+  RETURN NEW;
+END;
+$$;
+
+-- Trigger to create profile on user signup
+CREATE TRIGGER on_auth_user_created
+  AFTER INSERT ON auth.users
+  FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
