3a5b022a89e02b651ff9dab132c486a7b5c5f8d9
Jeffrey Plante <git@jplante.dev>
2025-12-09 10:22:27 -0800

    Create helper types

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -1,1 +1,43 @@
-// Implement helper types here
+// src/types/database.types.ts
+import { Database } from './supabase';
+
+// Extract table row types for easy access
+export type Profile = Database['public']['Tables']['profiles']['Row'];
+export type Message = Database['public']['Tables']['messages']['Row'];
+export type Conversation = Database['public']['Tables']['conversations']['Row'];
+export type ConversationParticipant = Database['public']['Tables']['conversation_participants']['Row'];
+
+// Types for inserts (useful for forms)
+export type ProfileInsert = Database['public']['Tables']['profiles']['Insert'];
+export type MessageInsert = Database['public']['Tables']['messages']['Insert'];
+export type ConversationInsert = Database['public']['Tables']['conversations']['Insert'];
+export type ConversationParticipantInsert = Database['public']['Tables']['conversation_participants']['Insert'];
+
+// Types for updates
+export type ProfileUpdate = Database['public']['Tables']['profiles']['Update'];
+export type MessageUpdate = Database['public']['Tables']['messages']['Update'];
+export type ConversationUpdate = Database['public']['Tables']['conversations']['Update'];
+export type ConversationParticipantUpdate = Database['public']['Tables']['conversation_participants']['Update'];
+
+// Composite types for queries with joins
+export type MessageWithSender = Message & {
+  sender: Profile;
+};
+
+export type MessageWithThread = Message & {
+  sender: Profile;
+  reply_to?: MessageWithSender | null;
+};
+
+export type ConversationWithParticipants = Conversation & {
+  participants: (ConversationParticipant & {
+    profile: Profile;
+  })[];
+};
+
+export type ConversationListItem = Conversation & {
+  participants: (ConversationParticipant & {
+    profile: Profile;
+  })[];
+  latest_message?: MessageWithSender;
+};
