1fdd410ccd750c23b8c003952b4aa28c1b6019ce
Jeffrey Plante <git@jplante.dev>
2025-12-12 12:44:36 -0800

    Remove boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,8 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
 
-function Copyright() {
-  return (
-    <Typography
-      variant="body2"
-      align="center"
-      sx={{
-        color: 'text.secondary',
-      }}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+
 
 export default function App() {
   return (
@@ -30,8 +12,6 @@ export default function App() {
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
           Material UI Vite example in TypeScript
         </Typography>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
