d954e96c63ce496444607c5bd93c8525091586a2
Jeffrey Plante <git@jplante.dev>
2025-12-12 13:11:34 -0800

    Install prettier

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -2,16 +2,12 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-
-
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Vite example in TypeScript
-        </Typography>
-      </Box>
-    </Container>
+    <Box sx={{ my: 4 }}>
+      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+        Material UI Vite example in TypeScript
+      </Typography>
+    </Box>
   );
 }
