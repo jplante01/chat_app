b917bbf60f2baa6d3180ee8db20a4e7722266394
Jeffrey Plante <git@jplante.dev>
2025-12-15 10:53:11 -0800

    Create and wire MainLayout

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,8 +1,7 @@
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-export default function App() {
+export default function MainLayout() {
   return (
     <Box sx={{ my: 4 }}>
       <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
