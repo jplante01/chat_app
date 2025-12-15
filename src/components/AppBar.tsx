62d99fc257d93ecacc4caed2a3275faf69447e2d
Jeffrey Plante <git@jplante.dev>
2025-12-15 13:43:58 -0800

    Organize components

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,37 @@
+import MuiAppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+interface AppBarProps {
+  drawerWidth: number;
+  onDrawerToggle: () => void;
+}
+
+export default function AppBar({ drawerWidth, onDrawerToggle }: AppBarProps) {
+  return (
+    <MuiAppBar
+      position="fixed"
+      sx={{
+        width: { sm: `calc(100% - ${drawerWidth}px)` },
+        ml: { sm: `${drawerWidth}px` },
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={onDrawerToggle}
+          sx={{ mr: 2, display: { sm: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          Responsive drawer
+        </Typography>
+      </Toolbar>
+    </MuiAppBar>
+  );
+}
---
+++ src/components/Drawer.tsx
@@ -0,0 +1,91 @@
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import MuiDrawer from '@mui/material/Drawer';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import Toolbar from '@mui/material/Toolbar';
+
+interface DrawerProps {
+  drawerWidth: number;
+  mobileOpen: boolean;
+  isClosing: boolean;
+  onDrawerClose: () => void;
+  onDrawerTransitionEnd: () => void;
+}
+
+const DrawerContent = () => (
+  <div>
+    <Toolbar />
+    <Divider />
+    <List>
+      {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+    <Divider />
+    <List>
+      {['All mail', 'Trash', 'Spam'].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  </div>
+);
+
+export default function Drawer({
+  drawerWidth,
+  mobileOpen,
+  isClosing,
+  onDrawerClose,
+  onDrawerTransitionEnd,
+}: DrawerProps) {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      aria-label="mailbox folders"
+    >
+      <MuiDrawer
+        variant="temporary"
+        open={mobileOpen}
+        onTransitionEnd={onDrawerTransitionEnd}
+        onClose={onDrawerClose}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        slotProps={{
+          root: {
+            keepMounted: true,
+          },
+        }}
+      >
+        <DrawerContent />
+      </MuiDrawer>
+      <MuiDrawer
+        variant="permanent"
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        open
+      >
+        <DrawerContent />
+      </MuiDrawer>
+    </Box>
+  );
+}
