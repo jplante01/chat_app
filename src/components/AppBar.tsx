8c466e97171983bbe540cee71e0792b593414578
Jeffrey Plante <git@jplante.dev>
2025-12-16 12:25:59 -0800

    Clean up error

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -3,7 +3,6 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import { ThemeToggle } from './common/ThemeToggle';
 
 interface AppBarProps {
